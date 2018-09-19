7651b0e8834e40bba8b5ed9aa750ae42a5027a51
GoodForNothing23 <43401640+GoodForNothing23@users.noreply.github.com>
2018-09-19 11:03:58 +0300

    vampire
    Add new mode of attack for new type of warrior: vampire.
    
    Co-Authored-By: goodfornothing23 <goodfornothing23@users.noreply.github.com>

--- war.py
+++ war.py
@@ -1,3 +1,5 @@
+# Taken from mission The Defenders
+
 # Taken from mission Army Battles
 
 # Taken from mission The Warriors
@@ -6,6 +8,8 @@ class Warrior:
     health = 50
     attack = 5
     defense = 0
+    vampirism = 0
+    
     @property
     def is_alive(self):
         if self.health > 0:
@@ -22,6 +26,11 @@ class Defender(Warrior):
     health = 60
     attack = 3
     defense = 2
+    
+class Vampire(Warrior):
+    health = 40
+    attack = 4
+    vampirism = 0.5
 
 
 class Army:
@@ -30,19 +39,11 @@ class Army:
         self.army = list()
     
     def add_units(self, s, n):
-        if s == Warrior:
-            self.quantity += n
-            for i in range(n):
-                self.army.extend([Warrior()])
+        self.quantity += n
+        for i in range(n):
+            self.army.extend([s()])
             
-        elif s == Knight:
-            self.quantity += n
-            for i in range(n):
-                self.army.extend([Knight()])
-        elif s == Defender:
-            self.quantity += n
-            for i in range(n):
-                self.army.extend([Defender()])
+        
     
     @property
     def is_full(self):
@@ -69,16 +70,15 @@ class Battle:
             
 def fight(unit_1, unit_2):
     j = True
-    i = 1
     while unit_1.is_alive and unit_2.is_alive:
-        if(i%2):
+        if unit_1.is_alive and unit_2.is_alive:
             if unit_2.defense < unit_1.attack:
                 unit_2.health -= unit_1.attack - unit_2.defense
-            i += 1
-        else:
-            if unit_1.defense < unit_2.attack:
-                unit_1.health -= unit_2.attack - unit_1.defense
-            i += 1
+                unit_1.health += (unit_1.attack - unit_2.defense)*unit_1.vampirism
+            if unit_2.is_alive:
+                if unit_1.defense < unit_2.attack:
+                    unit_1.health -= unit_2.attack - unit_1.defense
+                    unit_2.health += (unit_2.attack - unit_1.defense)*unit_2.vampirism
     if(unit_1.health > 0):
         j = True
     else:
@@ -162,6 +162,63 @@ if __name__ == '__main__':
     army_3.add_units(Defender, 1)
 
     army_4 = Army()
+    army_4.add_units(Warrior, 2)
+
+    battle = Battle()
+
+    assert battle.fight(my_army, enemy_army) == False
+    assert battle.fight(army_3, army_4) == True
+    print("Coding complete? Let's try tests!")
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    
+    #fight tests
+    chuck = Warrior()
+    bruce = Warrior()
+    carl = Knight()
+    dave = Warrior()
+    mark = Warrior()
+    bob = Defender()
+    mike = Knight()
+    rog = Warrior()
+    lancelot = Defender()
+    eric = Vampire()
+    adam = Vampire()
+    richard = Defender()
+    ogre = Warrior()
+
+    assert fight(chuck, bruce) == True
+    assert fight(dave, carl) == False
+    assert chuck.is_alive == True
+    assert bruce.is_alive == False
+    assert carl.is_alive == True
+    assert dave.is_alive == False
+    assert fight(carl, mark) == False
+    assert carl.is_alive == False
+    assert fight(bob, mike) == False
+    assert fight(lancelot, rog) == True
+    assert fight(eric, richard) == False
+    assert fight(ogre, adam) == True
+
+    #battle tests
+    my_army = Army()
+    my_army.add_units(Defender, 2)
+    my_army.add_units(Vampire, 2)
+    my_army.add_units(Warrior, 1)
+    
+    enemy_army = Army()
+    enemy_army.add_units(Warrior, 2)
+    enemy_army.add_units(Defender, 2)
+    enemy_army.add_units(Vampire, 3)
+
+    army_3 = Army()
+    army_3.add_units(Warrior, 1)
+    army_3.add_units(Defender, 4)
+
+    army_4 = Army()
+    army_4.add_units(Vampire, 3)
     army_4.add_units(Warrior, 2)
 
     battle = Battle()
